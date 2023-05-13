bf47209eaa19e90b69a745b5d64a23eb055c06c4
Tony Short <tshort15@ford.com>
2023-05-13 21:39:09 +0100

    Initial Commit

---
+++ AIAgent/AIAgentApp.swift
@@ -0,0 +1,20 @@
+//
+//  AIAgentApp.swift
+//  AIAgent
+//
+//  Created by Tony Short on 13/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct AIAgentApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
